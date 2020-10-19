582e28ad4ce38806e93152f4e6905d738f23d7bd
GuillaumeBe <guill.bettin@gmail.com>
2020-10-19 21:25:13 +0200

    fix responsiveness again

--- src/App/styles.ts
+++ src/App/styles.ts
@@ -9,7 +9,6 @@ export const Container = styled.div`
 export const Wrapper = styled.div`
   margin: 1rem;
   max-width: 35rem; 
-  width: 100%;
   border-radius: 6px;
   background-color: #fafafa;
   box-shadow: 0 0 6px -2px #00000080;
